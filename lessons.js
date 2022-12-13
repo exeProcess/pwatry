a2556a431faa8897036a458529312c5cded98a63
exeProcess <ajani_haeeb88@yahoo.com>
2022-12-13 17:59:42 +0100

    initial

---
+++ lessons.js
@@ -0,0 +1,56 @@
+let lessons = [
+  {
+    subject: "Computer Science",
+    description:
+      "This class is the best in the city! We offer anything you need 😉",
+    location: "The Burroughs, Hendon, London NW4 4BT",
+    price: 25,
+    spaces: 30,
+    img: "img/cs.jpg",
+  },
+  {
+    subject: "Maths",
+    description:
+      "This class is the best in the city! We offer anything you need 😉",
+    location: "The Burroughs, Hendon, London NW4 4BT",
+    price: 25,
+    spaces: 30,
+    img: "img/maths.svg",
+  },
+  {
+    subject: "English",
+    description:
+      "This class is the best in the city! We offer anything you need 😉",
+    location: "The Burroughs, Hendon, London NW4 4BT",
+    price: 25,
+    spaces: 30,
+    img: "img/english.png",
+  },
+  {
+    subject: "Science",
+    description:
+      "This class is the best in the city! We offer anything you need 😉",
+    location: "The Burroughs, Hendon, London NW4 4BT",
+    price: 25,
+    spaces: 30,
+    img: "img/science.png",
+  },
+  {
+    subject: "History",
+    description:
+      "This class is the best in the city! We offer anything you need 😉",
+    location: "The Burroughs, Hendon, London NW4 4BT",
+    price: 25,
+    spaces: 30,
+    img: "img/history.png",
+  },
+  {
+    subject: "Business",
+    description:
+      "This class is the best in the city! We offer anything you need 😉",
+    location: "The Burroughs, Hendon, London NW4 4BT",
+    price: 25,
+    spaces: 30,
+    img: "img/business.png",
+  },
+];
---
+++ service-worker.js
@@ -0,0 +1,46 @@
+let cacheName = "lessonsApp-v1";
+
+let cacheFiles = [
+  "index.html",
+  "lessons.js",
+  "lessonsApp.webmanifest",
+  "img/icon.png",
+  "img/cs.jpg",
+  "img/maths.svg",
+  "img/english.png",
+  "img/science.png",
+  "img/history.png",
+  "img/business.png",
+];
+
+self.addEventListener("install", (e) => {
+  console.log("[Service Worker] Install Learning App");
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      console.log("[Service Worker] Caching all the files");
+      return cache.addAll(cacheFiles);
+    })
+  );
+});
+
+self.addEventListener("fetch", function (e) {
+  /*
+  if (isCrossOrigin(e.request)) {
+    return;
+  }*/
+  e.respondWith(
+    caches.match(e.request).then(function (r) {
+      // Download the file if it is not in the cache,
+      return (
+        r ||
+        fetch(e.request).then(function (response) {
+          // add the new file to cache
+          return caches.open(cacheName).then(function (cache) {
+            cache.put(e.request, response.clone());
+            return response;
+          });
+        })
+      );
+    })
+  );
+});
